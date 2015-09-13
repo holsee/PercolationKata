71093fdd87550184fdf7f24bedb2c95f3392da9c
Chris McGrath <chris@chrismcg.com>
2015-09-13 17:31:20 +0100

    Make tests async and add a couple more

--- test/percolation_test.exs
+++ test/percolation_test.exs
@@ -1,7 +1,7 @@
 defmodule PercolationTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "returns true when there's a path through the material" do
+  test "returns true when there's a path through the material 1" do
     material = [
       [1, 0, 1, 1, 0],
       [1, 0, 0, 1, 1],
@@ -12,12 +12,34 @@ defmodule PercolationTest do
     assert Percolation.permeable?(material)
   end
 
-  test "returns false when there's isn't a path through the material" do
+  test "returns true when there's a path through the material 2" do
+    material = [
+      [1, 1, 0, 1, 0],
+      [0, 0, 0, 1, 1],
+      [0, 1, 0, 1, 1],
+      [0, 1, 0, 0, 0],
+      [1, 0, 1, 1, 0]
+    ]
+    assert Percolation.permeable?(material)
+  end
+
+  test "returns false when there's isn't a path through the material 1" do
     material = [
       [1, 0, 1, 1, 0],
       [1, 1, 0, 1, 1],
       [1, 1, 0, 1, 1],
       [1, 0, 0, 1, 1],
+      [1, 0, 1, 1, 1]
+    ]
+    refute Percolation.permeable?(material)
+  end
+
+  test "returns false when there's isn't a path through the material 2" do
+    material = [
+      [1, 1, 0, 1, 0],
+      [0, 0, 0, 1, 1],
+      [1, 1, 0, 1, 1],
+      [1, 1, 0, 1, 1],
       [1, 0, 1, 1, 1]
     ]
     refute Percolation.permeable?(material)
